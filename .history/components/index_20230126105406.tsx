95b6431737e210747a274a08c6593e4a159cd7a2
Federico Ramallo <fjramallo.1999@hotmail.com>
2023-01-26 10:54:33 -0300

    PreBuild

---
+++ .history/components/index_20230126105406.tsx
@@ -0,0 +1,9 @@
+export { default as SideBar } from './SideBar';
+export { default as Categoria } from './Categoria';
+export { default as Layout } from './Layout';
+export { default as Producto } from './Producto';
+export { default as ModalProducto } from './ModalProducto';
+export { default as Pasos } from './Pasos';
+export { default as ResumenProducto } from './ResumenProducto';
+export { default as AdminLayout } from './AdminLayout';
+export { default as Orden } from './Orden';
